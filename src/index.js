5af61b1b77d6199e99a28d1c053f3d312ceeb221
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-03-29 19:36:55 +0900

    feat: プロジェクトの初期構成に適応

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// ReactDOM.render()を使った書き方は旧型式化
+// React 18 では先にDOMの情報を渡してからrender()する形に変更
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <StrictMode>
+        <App title="App"
+            message="this is App Component" />
+    </StrictMode>,
+)
