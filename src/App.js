1c2ab6e7502ff4f7f5b0f86db634efe1508199db
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-03-30 17:59:57 +0900

    feat: 簡易メモアプリの実装

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
-import React, { Component } from "react"
 import './App.css'
+import MemoPage from "./memo/MemoPage"
 
-class App extends Component {
-    input = ""
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            title: "input form",
-            message: "type your name"
-        }
-        this.doChange = this.doChange.bind(this)
-        this.doSubmit = this.doSubmit.bind(this)
-    }
-
-    doChange(event) {
-        this.input = event.target.value
-    }
-
-    doSubmit(event) {
-        this.setState({
-            title: "send form", 
-            message: "Hello, " + this.input + "!!"
-        })
-        event.preventDefault()
-    }
-
-    render() {
-        return  <div>
+function App() {
+    return (
+        <div>
             <h1 className="bg-primary text-white display-4">React</h1>
             <div className="container">
-                <h4>{this.state.title}</h4>
-                <p className="card h5 p-3">{this.state.message}</p>
-                <div className="alert alert-primary mt-3">
-                    <form onSubmit={this.onSubmit}>
-                        <div className="form-group">
-                            <label>Message:</label>
-                            <input type="text" className="form-control" onChange={this.doChange} />
-                        </div>
-                        <input type="submit" className="btn btn-primary" value="Click" />
-                    </form>
-                </div>
+                <h4 className="my-3">Memo.</h4>
+                    <MemoPage />
             </div>
         </div>
-    }
+    )
 }
 
 export default App
