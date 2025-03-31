bd67162ef63c43e212fcef84e5e3a495384524bc
mimisukeMaster <81568941+mimisukeMaster@users.noreply.github.com>
2025-03-31 18:01:45 +0900

    update: AppRouterを使わない初期構成

---
+++ src/pages/index.js
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
+
+export default function Home() {
+    let title = "Next.js page"
+    let message = "React Next.js sample page."
+
+    return (
+        <div>
+            <Head>
+                <title>{title}</title>
+                <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/
+                    bootstrap/4.5.0/css/bootstrap.min.css"
+                    crossOrigin="anonymous"></link>
+            </Head>
+
+            <h1 className="bg-primary text-white display-4 ">React</h1>
+        </div>
+    )
+}
